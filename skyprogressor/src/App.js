e807a6986fdbaa1916e0d95254164ae527da7a6c
Ryan Guan <ryanguan0000@gmail.com>
2026-07-09 18:13:51 -0400

    dxt

--- skyprogressor/src/App.js
+++ skyprogressor/src/App.js
@@ -2,18 +2,13 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const TITLE = "skyProgressor";
-  const DESC = "AI-powered Hypixel Skyblock Assistant";
-  const SB_PROFILE = "https://sky.shiiyu.moe/stats/westkorean/Coconut#Gear";
+  const title = "skyProgressor";
 
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <h3>{DESC}</h3>
-        
-        <a href={SB_PROFILE}>My Profile</a>
       </div>
     </div>
   );
--- skyprogressor/src/Home.js
+++ skyprogressor/src/Home.js
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [name, setName] = useState("westkorean");
-    const [id, setID] = useState(110023923902903);
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
-    const handleClick = () => {
-       setName("ThirtyVirus");
-       setID(110023923902904);
+    const [name, setName] = useState('mario');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id != id);
+        setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            <BlogList blogs={blogs} handleDelete={handleDelete} title="Latest Blogs" />
+            <button onClick={() => setName('luigi')}>change name</button>
             <p>{name}</p>
-            <p>{id}</p>
-            <button onClick={handleClick}>Get Started</button>
         </div>
      );
 }
