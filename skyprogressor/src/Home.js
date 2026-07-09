6782914651ed7ffe72a0a13be210f55456344b90
Ryan Guan <ryanguan0000@gmail.com>
2026-07-09 18:41:54 -0400

    dxt

--- skyprogressor/src/Home.js
+++ skyprogressor/src/Home.js
@@ -2,29 +2,26 @@ import { useState, useEffect } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id != id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data =>{
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+        }, 1000);
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} handleDelete={handleDelete} title="Latest Blogs" />
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{name}</p>
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="Latest Blogs" />}
         </div>
      );
 }
