ad1f9aa67a0962d2c459081c17e2d8c043f692ab
Ryan Guan <ryanguan0000@gmail.com>
2026-07-10 17:41:47 -0400

    dxt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import BlogDetails from './BlogsDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />}/>
             <Route path="/create" element={<Create />}/>
+            <Route path="/blogs/:id" element={<BlogDetails />}/>
           </Routes>
         </div>
       </div>
